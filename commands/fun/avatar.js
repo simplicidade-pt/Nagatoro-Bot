0eaf1aafbd3c95b45573d2493d863ca602d3c5f6
Scxipted <scxipted@gmail.com>
2022-01-08 23:14:22 +0000

    Update avatar.js

--- commands/fun/avatar.js
+++ commands/fun/avatar.js
@@ -8,13 +8,14 @@ module.exports = {
   usage: "avatar <@user>",
   run: async (client, message, args) => {
     if (message.author.bot) return;
+
     const Target = message.mentions.users.first()
 
     if (Target) {
       let avatarEmbed = new Discord.MessageEmbed()
         .setColor(colors.info)
         .setTitle("Your avatar:")
-        .setImage(Target.author.displayAvatarURL({ dynamic: true }))
+        .setImage(Target.user.author.displayAvatarURL({ dynamic: true }))
         .setFooter("Requested by " + message.member.user.tag)
         .setTimestamp();
       message.reply({ embeds: [avatarEmbed] });
@@ -23,7 +24,7 @@ module.exports = {
         .setColor(colors.info)
         .setTimestamp()
         .setTitle(message.member.user.name + "'s Avatar:")
-        .setImage(message.author.displayAvatarURL({ dynamic: true }))
+        .setImage(message.user.author.displayAvatarURL({ dynamic: true }))
         .setFooter("Requested by " + message.member.user.tag);
 
       message.reply({ embeds: [avatarEmbed] });
