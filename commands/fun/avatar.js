4d46c790c18cbc415b5e08141c4644415684d374
Antonio B <scxipted@gmail.com>
2022-02-19 15:22:24 +0000

    Update avatar.js

--- commands/fun/avatar.js
+++ commands/fun/avatar.js
@@ -2,34 +2,21 @@ const Discord = require("discord.js");
 const colors = require("../../configuration/colors.json");
 
 module.exports = {
-  name: "avatar" || "av",
+  name: "avatar",
   category: "fun",
   description: "Get a users avatar",
   usage: "avatar <@user>",
   run: async (client, message, args) => {
     if (message.author.bot) return;
 
-    const Target = message.mentions.users.first()
-
-    if (Target) {
-      let avatarEmbed = new Discord.MessageEmbed()
+    const Target = message.mentions.users.first() || message.member.user
+    let avatarEmbed = new Discord.MessageEmbed()
         .setColor(colors.info)
-        .setTitle("Your avatar:")
-        .setImage(Target.user.author.displayAvatarURL({ dynamic: true }))
+        .setTitle(Target.username + "'s Avatar")
+        .setImage(Target.displayAvatarURL({ size: 4096, format: "png"}))
         .setFooter("Requested by " + message.member.user.tag)
         .setTimestamp();
-      message.reply({ embeds: [avatarEmbed] });
-      
-    } else {
-
-      let avatarEmbed = new Discord.MessageEmbed()
-        .setColor(colors.info)
-        .setTimestamp()
-        .setTitle(message.member.user.name + "'s Avatar:")
-        .setImage(message.author.displayAvatarURL({ dynamic: true }))
-        .setFooter("Requested by " + message.member.user.tag);
 
       message.reply({ embeds: [avatarEmbed] });
-    }
   },
 };
